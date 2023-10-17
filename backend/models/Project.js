940c2d25414ecbabb1cc6d8d08b75ae28dc7457b
Lecuyer Quentin <Quentin.Lecuyer@mailfence.com>
2023-10-17 16:38:57 +0200

    Correction shemaProject

--- backend/models/Project.js
+++ backend/models/Project.js
@@ -2,35 +2,22 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const projectSchema = new Schema({
-    name: {
+    title: {
         type: String,
         required: true,
         unique: true,
     },
     description: {
         type: String,
-        require: true,
+        required: true,
     },
     owner: {
-        // type: Schema.Types.ObjectId,
-        // ref: 'User',
         type: String,
         default: '',
-
     },
-    image: {
+    images: {
         type: Array,
-
-        default: [
-            defaultImage = {
-                'url': 'https://images.unsplash.com/photo-1542744095-291d1f67b221?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvamVjdCUyMHBsYXllciUyMHNob3BwaW5nJTIwY29sb3JmdWwlMjBzdGF0aW9ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
-                'pub': 'https://unsplash.com/@paulgilmore_',
-                'alt': 'default image'
-            }
-        ],
-
-        // type: String,
-        // default: 'https://images.unsplash.com/photo-1542744095-291d1f67b221?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvamVjdCUyMHBsYXllciUyMHNob3BwaW5nJTIwY29sb3JmdWwlMjBzdGF0aW9ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80',
+        default: [],
     },
     technologies: {
         type: Array,
@@ -44,7 +31,7 @@ const projectSchema = new Schema({
         type: Date,
         default: Date.now,
     },
-    updatedAt: Date,
+    // updatedAt: Date,
 },
     {
         timestamps: true,
