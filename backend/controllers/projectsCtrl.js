740308998e254f5a24111d9c12be36462b22d2c0
Lecuyer Quentin <Quentin.Lecuyer@mailfence.com>
2023-10-17 16:37:11 +0200

    Correction addProject

--- backend/controllers/projectsCtrl.js
+++ backend/controllers/projectsCtrl.js
@@ -14,131 +14,41 @@ const getProjectById = async (req, res) => {
     const project = await Project.findOne({ _id: req.params.id }).exec()
     if (!project) return res.status(204).json({ 'message': 'No project found' })
     try {
-        res.json(project)
+        res.status(200).json( project )
     } catch (err) {
         res.status(500).json({ 'message': err.message })
     }
 }
 
-
 const createProject = async (req, res) => {
-    const project = JSON.parse(req.body.project)
-
-    console.log("project CreateProject", project)
-    console.log("req.files CreateProject", req.files)
-    console.log("req.body.project CreateProject", req.body.project)
-
-    delete project._id
-    // delete project._userId
-
-    const newProject = new Project({
-        name: project.name,
-        description: project.description,
-        owner: project.owner,
-        images: [
-            {
-                url: req.files[0].path,
-                alt: req.files[0].filename
-            }
-        ],
-        technologies: project.technologies,
-        link: project.link,
-        createdAt: project.createdAt,
-        updatedAt: project.updatedAt
-    })
-
+    const { title, description, owner, technologies, link } = req.body
+    if (!title || !description || !owner) return res.status(400).json({ 'message': 'Missing required fields' })
     try {
-        const result = await newProject.save()
-        res.status(201).json({ 'success': `Project ${project.name} created successfully` })
+        const existingProject = await Project.findOne({ title: title }).exec()
+        if (existingProject) return res.status(400).json({ 'message': 'Project already exists' })
+
+        const project = new Project({
+            title,
+            description,
+            owner,
+            technologies,
+            link,
+            createdAt: new Date().toISOString(),
+        })
+        const result = await project.save()
+        res.status(201).json({ 'success': `Project ${title} created successfully` })
     } catch (err) {
-        res.status(400).json({ 'message': err.message })
+        res.status(500).json({ 'message': err.message })
     }
- }
-
-
-
-
-// const createProject = async (req, res) => {
-//     const { name, description, owner, link, images:[], technologies:[], createdAt, updatedAt } = req.body
-//     // const { name, description, owner, link, createdAt, updatedAt } = req.body
-//     if (!name || !description) return res.status(400).json({ 'message': 'Missing required fields' })
-    
-//     const project = new Project({
-//         name: name,
-//         description: description,
-//         owner: owner,
-//         images: [
-//             {
-//                 url: '',
-//                 alt: ''
-//             },
-//         ],
-//         technologies: [
-//             {
-//                 name: ''
-//             },
-//         ],
-//         link: link,
-//         createdAt: createdAt,
-//         updatedAt: updatedAt
-//     })
-//     try {
-//         const result = await project.save()
-//         res.status(201).json({ 'success': `Project ${name} created successfully` })
-//     } catch (err) {
-//         res.status(500).json({ 'message': err.message })
-//     }
-// }
-
-// const createProject = async (req, res) => {
-//     if (!req?.body?.formData) return res.status(400).json({ 'message': 'No project data provided' })
-//     new Project({
-//         formData: {
-//             name: name,
-//             description: description,
-//             owner: owner,
-//             images: images,
-//             technologies: technologies,
-//             link: link,
-//             createdAt: createdAt,
-//             updatedAt: updatedAt
-//         }
-//     }).save((err) => {
-//         if (err) return res.status(500).json({ 'message': err.message })
-//         res.status(201).json({ 'success': `Project ${name} created successfully` })
-//     })
-// }
-
-// const createProject = async (req, res) => {
-//     if (!req?.body?.formData) return res.status(400).json({ 'message': 'No project data provided' })
-//     const { name, description, owner, images, technologies, link, createdAt, updatedAt } = req.body.formData
-//     if (!name || !description) return res.status(400).json({ 'message': 'Missing required fields' })
-//     const project = new Project({
-//         name: name,
-//         description: description,
-//         owner: owner,
-//         images: images,
-//         technologies: technologies,
-//         link: link,
-//         createdAt: createdAt,
-//         updatedAt: updatedAt
-//     })
-//     try {
-//         const result = await project.save()
-//         res.status(201).json({ 'success': `Project ${name} created successfully` })
-//     } catch (err) {
-//         res.status(500).json({ 'message': err.message })
-//     }
-// }
+}
 
 const updateProject = async (req, res) => {
     if (!req?.params?.id) return res.status(400).json({ 'message': 'No project id provided' })
-    const { name, description, owner, images, technologies, link } = req.body
-    // if (!name || !description || !owner ) return res.status(400).json({ 'message': 'Missing required fields' })
+    const { title, description, owner, images, technologies, link } = req.body
     const project = await Project.findOne({ _id: req.params.id }).exec()
     if (!project) return res.status(204).json({ 'message': 'No project found' })
 
-    if (req.body?.name) return project.name = name
+    if (req.body?.title) return project.name = title
     if (req.body?.description) return project.description = description
     if (req.body?.owner) return project.owner = owner
     if (req.body?.images) return project.images = images
@@ -147,7 +57,7 @@ const updateProject = async (req, res) => {
     
     try {
         const result = await project.save()
-        res.status(200).json({ 'success': `Project ${name} updated successfully` })
+        res.status(200).json({ 'success': `Project ${title} updated successfully` })
     } catch (err) {
         res.status(500).json({ 'message': err.message })
     }
