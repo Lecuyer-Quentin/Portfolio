32f7c7af864dd18128342a4b5e8bb79973a7ba77
Lecuyer Quentin <Quentin.Lecuyer@mailfence.com>
2023-10-17 16:42:07 +0200

    Add upload route

--- backend/routes/api/projects.js
+++ backend/routes/api/projects.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const projectsCtrl = require('../../controllers/projectsCtrl');
-// const multer = require('../../config/multer');
 const upload = require('../../config/multer');
-// const ROLES_LIST = require('../../config/roleList');
-// const verifyRoles = require('../../middlewares/verifyRoles');
-//! verifyRoles(ROLES_LIST.Admin) don't work
+const uploadCtrl = require('../../controllers/uploadCtrl');
 const verifyJWT = require('../../middlewares/verifyJWT');
 
 
 router.route('/')
     .get(projectsCtrl.getAllProjects)
-    .post(verifyJWT, upload.array('images', 10), projectsCtrl.createProject);
+    .post(verifyJWT, projectsCtrl.createProject)
 
 router.route('/:id')
     .get(projectsCtrl.getProjectById)
-    .put(verifyJWT, projectsCtrl.updateProject) // verifyJWT,
-    .delete(verifyJWT, projectsCtrl.deleteProject);// verifyJWT,
+    .put(verifyJWT, projectsCtrl.updateProject)
+    .delete(verifyJWT, projectsCtrl.deleteProject)
+    .post(verifyJWT, upload.array('images', 10), uploadCtrl.uploadImages);
+
 
 module.exports = router;
