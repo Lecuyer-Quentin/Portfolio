900128f290ae5232d90f477655764ed0fcff5678
Lecuyer Quentin <Quentin.Lecuyer@mailfence.com>
2023-10-19 18:14:57 +0200

    correction routes projects

--- backend/routes/api/projects.js
+++ backend/routes/api/projects.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const projectsCtrl = require('../../controllers/projectsCtrl');
-const upload = require('../../config/multer');
-const uploadCtrl = require('../../controllers/uploadCtrl');
+// const upload = require('../../config/multer');
+// const uploadCtrl = require('../../controllers/uploadCtrl');
 const verifyJWT = require('../../middlewares/verifyJWT');
+
+const multer = require('multer');
+
+const parse = multer();
 
 
 router.route('/')
     .get(projectsCtrl.getAllProjects)
-    .post(verifyJWT, projectsCtrl.createProject)
+    .post(verifyJWT, parse.none(), projectsCtrl.createProject)
 
 router.route('/:id')
     .get(projectsCtrl.getProjectById)
     .put(verifyJWT, projectsCtrl.updateProject)
     .delete(verifyJWT, projectsCtrl.deleteProject)
-    .post(verifyJWT, upload.array('images', 10), uploadCtrl.uploadImages);
+    // .post(verifyJWT, upload.array('images', 10), uploadCtrl.uploadImages);
 
 
 module.exports = router;
