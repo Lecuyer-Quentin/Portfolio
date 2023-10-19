39a3321a56c5617ad8ffd11669ea19c5916bae40
Lecuyer Quentin <Quentin.Lecuyer@mailfence.com>
2023-10-19 18:21:32 +0200

    Maj store

--- frontend/src/app/store.jsx
+++ frontend/src/app/store.jsx
@@ -2,21 +2,23 @@ import { configureStore } from '@reduxjs/toolkit';
 import { authApi, authSlice } from './api/authSlice';
 import { projectsSlice } from './api/projectsSlice';
 import { usersSlice } from './api/usersSlice';
+import { imagesSlice } from './api/imagesSlice';
 
 
 export const store = configureStore({
     reducer: {
-        // auth: authReducer,
         auth: authSlice.reducer,
         authApi: authApi.reducer,
         projects: projectsSlice.reducer,
         users: usersSlice.reducer,
+        images: imagesSlice.reducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(
             authApi.middleware,
             projectsSlice.middleware,
             usersSlice.middleware,
+            imagesSlice.middleware,
         ),
   
 });
